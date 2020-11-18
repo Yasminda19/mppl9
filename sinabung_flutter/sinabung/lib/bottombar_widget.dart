0c145f7bccde7559aa2ef5808ab90ed90ba76362
Naufal Shidqi <naufal_14199@apps.ipb.ac.id>
2020-11-18 21:29:27 +0700

    Divide Card Widgets, First Attempt Interactive Navbar

---
+++ sinabung_flutter/sinabung/lib/bottombar_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class MyBottomBar extends StatefulWidget {
+  @override
+  _MyBottomBarState createState() => _MyBottomBarState();
+}
+
+class _MyBottomBarState extends State<MyBottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Flutter App'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: 0, // this will be set when a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home_sharp),
+            label: 'Home'
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.insert_chart_sharp),
+            label: 'Analisis'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box_sharp),
+            label: 'Tambah'
+          ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.restore_sharp),
+            label: 'History'
+            )
+        ],
+      ),
+    );
+  }
+}
+
+// BottomNavigationBarItem(
+// icon: new Icon(Icons.home_sharp),
+// label: 'Home',
+// ),
+// BottomNavigationBarItem(
+// icon: new Icon(Icons.insert_chart_sharp),
+// label: 'Analisis',
+// ),
+// BottomNavigationBarItem(
+// icon: new Icon(Icons.add_box_sharp),
+// label: 'Tambah',
+// ),
+// BottomNavigationBarItem(
+// icon: new Icon(Icons.restore_sharp),
+// label: 'History',
--- sinabung_flutter/sinabung/lib/main.dart
+++ sinabung_flutter/sinabung/lib/main.dart
@@ -1,147 +1,21 @@
 import 'package:flutter/material.dart';
+import 'card_widget.dart';
+import 'bottombar_widget.dart';
 
-void main() => runApp(MyApp());
 
-class MyBottomBar extends StatefulWidget {
-  @override
-  _MyBottomBarState createState() => _MyBottomBarState();
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
 
-  Widget mainCards = ListView(
-    children:
-    const <Widget>[
-      Card(
-        child: ListTile(
-          title: Text('Pengeluaran Bulan Ini'),
-          subtitle: Text.rich(
-            TextSpan( // default text style
-              children: <TextSpan>[
-                TextSpan(text: 'Rp.1.800.500', style: TextStyle(
-                    fontSize: 32, fontWeight: FontWeight.bold, color: Colors.black)
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      Card(
-        child: ListTile(
-          leading: Icon(Icons.local_atm),
-          title: Text('Pemasukkan'),
-          trailing: Text.rich(
-            TextSpan( // default text style
-              children: <TextSpan>[
-                TextSpan(text: 'Rp.800.500', style: TextStyle(
-                  color: Colors.black)
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      Card(
-        child: ListTile(
-          leading: Icon(Icons.account_balance_wallet),
-          title: Text('Net'),
-          trailing: Text.rich(
-            TextSpan( // default text style
-              children: <TextSpan>[
-                TextSpan(text: 'Rp.650.000', style: TextStyle(
-                    color: Colors.black)
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      Card(
-        child: ListTile(
-          leading: Icon(Icons.monetization_on),
-          title: Text('Tabungan'),
-        ),
-      )
-    ],
-  );
-
-  Widget buttonSection = Container(
-    margin: const EdgeInsets.all(8),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.home),
-          ],
-        ),
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.insert_chart_sharp),
-          ],
-        ),
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.add_box_sharp),
-          ],
-        ),
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.restore),
-          ],
-        )
-      ],
-    ),
-  );
-  Row _buildButtonRow(IconData icon, String label) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon),
-        Container(
-          margin: const EdgeInsets.only(left: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sinabung',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Sinabung'),
-        ),
-        body: mainCards,
-        bottomNavigationBar: BottomAppBar(
-          child: buttonSection,
-        )
-      ),
+      home: MyBottomBar()
     );
   }
 }
 
-class _MyBottomBarState extends State<MyBottomBar> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+
 
 
